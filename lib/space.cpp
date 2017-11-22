9b184bda704ddcfa4c25a466c1532280cfe877a5
James de Lisle <lpko908@gmail.com>
2017-11-22 12:05:15 +0900

    debug

--- lib/space.cpp
+++ lib/space.cpp
@@ -57,7 +57,7 @@ void Space::run(std::string _data_folder) {
   int i, j, k;
   InData inData(_data_folder, order, lim);
   int max_threads = omp_get_max_threads();
-  omp_set_num_threads(max_threads-3);
+  omp_set_num_threads(max_threads);
   std::cout << "Computing order: " << order << "..." << std::endl;
   for (i=0; i<lim.energyN; i++) {
     for (j=0; j<lim.kPolarN; j++) {
