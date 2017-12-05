8bff5aa2f6cf00445b8d72764ec2ec7e0bc07e77
James de Lisle <lpko908@gmail.com>
2017-12-05 17:14:49 +0900

    perhaps

--- test/main.cpp
+++ test/main.cpp
@@ -38,8 +38,9 @@ int main(int argc, char * argv[]) {
   std::ofstream TRACKD;
   std::ifstream CHECK;
 
-  std::vector<double> MFIELD = SPACE::linspace(0.3, 0.0, 10); 
-  std::vector<double> TEMPIN = SPACE::linspace(0.03, 0.0, 10); 
+  //std::vector<double> MFIELD = SPACE::linspace(0.3, 0.0, 10); 
+  //std::vector<double> TEMPIN = SPACE::linspace(0.03, 0.0, 10); 
+  std::vector<double> ANIS = SPACE::linspace(0.5, 0.0, 10); 
   
   l.energyN = 100;
   l.kPolarN = 50;
@@ -55,8 +56,8 @@ int main(int argc, char * argv[]) {
   l.alphaMax = 0.0;
   l.temp = 0.02;
   l.tempCrit = 0.1;
-  l.a1 = 0.135;
-  l.a2 = 0.22;
+  l.a1 = 0.0;
+  l.a2 = 0.0;
   l.a3 = 0.48;
   l.a4 = -0.242;
   l.tau = 0.01;
@@ -67,7 +68,7 @@ int main(int argc, char * argv[]) {
   l.start_time = _start_time; 
   int i, j, order, max_order = 6;
   for (i=0; i<10; i++) {
-    l.tempInc = TEMPIN[i];
+    l.a1 = ANIS[i];
     for (j=0; j<10; j++) {
       if (l.spin) {
 	std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -79,10 +80,10 @@ int main(int argc, char * argv[]) {
 	TRACKD.open((const char *) (data_folder + "TRACKD").c_str());
 	TRACKD.close();
       }
-      l.magF = MFIELD[j];
+      l.a2 = ANIS[j];
       Limits L(l);
       L.save(data_folder);
-      std::cout << L.magF << " " << L.tempInc << std::endl;
+      std::cout << L.a1 << " " << L.a2 << std::endl;
       for (order=0; order<max_order; order++) {
 	Space S(L, _start_time, order);
 	S.run(data_folder);
