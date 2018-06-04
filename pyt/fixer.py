2ded9400c0f5b4d32b32a14ce7eab90d75cb3c77
James de Lisle <lpko908@gmail.com>
2018-06-04 10:53:52 +0900

    new start

---
+++ pyt/fixer.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import time
+import os
+import sys
+
+
+def check(folder):
+    command = "ls -l %s | grep TRACKU | cut -c46-50" % folder
+    print command
+    print os.popen(command).read().split(":")
+    t = int(os.popen(command).read().split(":")[0])
+    tdiff = time.localtime().tm_hour - t
+    if tdiff > 1:
+        os.remove("%sTRACKU" % folder)
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    check(sys.argv[1])
