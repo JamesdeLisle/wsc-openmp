d1974f8c54d5cf9ef64e51b885192a387ccf06ad
James de Lisle <lpko908@gmail.com>
2018-06-04 13:18:36 +0900

    new start

--- test/main.cpp
+++ test/main.cpp
@@ -54,17 +54,17 @@ int main(int argc, char * argv[]) {
   l.start_time = time; 
 
   int disc = 25;
-  vector<double> A = SPACE::linspace(0.0, 1.0, disc);
-  vector<double> B = SPACE::linspace(0.0, 1.0, disc);
+  vector<double> A = SPACE::linspace(0.0, 0.3, disc);
+  vector<double> B = SPACE::linspace(0.0, 0.03, disc);
   
   Timer T = Timer();
   ENG.setThreads(n_threads);
   
   int i, j;
-  for (i=0; i<disc; i++) {
-    l.a1 = A[i];
-    for (j=0; j<disc; j++) {
-      l.a2 = B[j];
+  for (i=5; i<disc; i++) {
+    l.magF = A[i];
+    for (j=17; j<disc; j++) {
+      l.tempInc = B[j];
       ENG.run(l, max_order, suffix);
     }
   }
