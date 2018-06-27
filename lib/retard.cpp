ddb1558d87ae2af2ef8271be46f9446c007b7be8
James de Lisle <lpko908@gmail.com>
2018-06-27 13:09:04 +0900

    new start

--- lib/retard.cpp
+++ lib/retard.cpp
@@ -12,11 +12,11 @@ Retarded::Retarded(RunVal _runVal, mat _dg0) : runVal(_runVal), dg0(_dg0) {
 }
 
 Green Retarded::get() {
-  Green rv, tmp;
+  Green rv;
   std::complex<double> I(0.0, 1.0);
   double sgn, pi = M_PI;
   Environment E(runVal);
-  tmp = E.epsi() - E.hamR();
+  rv = E.epsi() - E.hamR();
   if (runVal.getXi() < pi / 2) {
     sgn = 1.0;
   }
@@ -24,9 +24,6 @@ Green Retarded::get() {
     sgn = -1.0;
   }
   
-  rv.set(0.5 * (-1.0 / ((runVal.getE() - E.getSigmaR()) *
-			(runVal.getE() - E.getSigmaR()) -
-			E.getDeltaR() * std::conj(E.getDeltaR()))) *	\
-	 tmp.get().inverse() * sgn * runVal.getlim()->magF * I * dg0);
+  rv.set(0.5 * rv.get().inverse() * sgn * runVal.getlim()->magF * I * dg0);
   return rv;
 }
