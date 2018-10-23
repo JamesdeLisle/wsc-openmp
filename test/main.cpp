8d4c1deeba854a421e0bf37d55c7f80dc52369cf
James de Lisle <lpko908@gmail.com>
2018-10-23 18:13:04 +0900

    highdisc

--- test/main.cpp
+++ test/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char * argv[]) {
   l.tau = 0.01;
   l.fermVU = 1.1;
   l.fermVD = 1.0;
-  l.tempInc = 0.015;
+  l.tempInc = 0.01 / l.alphaMax;
   l.magF = 0.3;
   l.start_time = time; 
 
