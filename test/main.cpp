bfb2922f255b7de413fc5060574f941a8c555910
James de Lisle <lpko908@gmail.com>
2018-10-27 12:13:08 +0900

    highdisc

--- test/main.cpp
+++ test/main.cpp
@@ -68,6 +68,7 @@ int main(int argc, char * argv[]) {
   Timer T = Timer();
   ENG.setThreads(n_threads);
   l.alphaN = 100;
+ 
   ENG.run(l, max_order, suffix, flogp);
   //l.alphaN = 300;
   //ENG.run(l, max_order, suffix, flogp);
